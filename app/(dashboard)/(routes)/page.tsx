783b38c501ba2cc6bbbcf3a244463e85388bac9a
IvanMilicevic <ivanmilicevicmeda@gmail.com>
2023-10-05 19:54:14 +0200

    Authentication

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <div className="text-red-500">
+      <Button>Hello</Button>
+    </div>
+  );
+}
